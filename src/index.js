5b42cf5c928b90845b6eedb0a39c9cbccab550a7
smit <smit@gmail.com>
2025-02-20 18:15:39 +0530

    remove extra space

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ connectDB()
     })
     app.listen(process.env.PORT || 8000, ()=> {
         console.log(`server is running at port : ${process.env.PORT}`);
-        
     })
 })
 .catch((error)=>{
