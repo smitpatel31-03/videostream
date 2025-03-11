93cbe3dd9dd662f8f1e2a6e148e969a1d9140f51
smit <smit@gmail.com>
2025-03-11 11:26:55 +0530

    add user avatar and cover image update routes; implement user details retrieval and watch history endpoints

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { loginUser, logoutUser, registerUser, refreshAccessToken,subscibedToChannel,getchannelDetails,updateAccountDetails,changeCurruntUserPassword } from "../controllers/user.controller.js"
+import { loginUser, logoutUser, registerUser, refreshAccessToken, subscibedToChannel, getchannelDetails, updateAccountDetails, changeCurruntUserPassword, updateUserAvatar, updateUserCoverImage, getCurruntUser, getWatchHistory } from "../controllers/user.controller.js"
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
 
@@ -25,10 +25,19 @@ router.route("/login").post(loginUser)
 //secured routs
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
-router.route("/updateAccountDetails").post(verifyJWT,updateAccountDetails)
-router.route("/changeCurruntUserPassword").post(verifyJWT,changeCurruntUserPassword)
-router.route("/subscibedToChannel/:channelId").post(verifyJWT,subscibedToChannel)
-router.route("/getchannelDetails/:username").get(verifyJWT,getchannelDetails)
+router.route("/changeCurruntUserPassword").post(verifyJWT, changeCurruntUserPassword)
+router.route("/subscibedToChannel/:channelId").post(verifyJWT, subscibedToChannel)
+
+//patch Route
+router.route("/updateAccountDetails").patch(verifyJWT, updateAccountDetails)
+router.route("/updateUserAvatar").patch(verifyJWT, upload.single("avatar"),updateUserAvatar)
+router.route("/updateUserCoverImage").patch(verifyJWT, upload.fields("coverImage"),updateUserCoverImage)
+
+//get route
+router.route("/getchannelDetails/:username").get(verifyJWT, getchannelDetails)
+router.route("/getCurruntUser").get(verifyJWT, getCurruntUser)
+router.route("/getWatchHistory").get(verifyJWT, getWatchHistory)
+
 
 
 export default router 
